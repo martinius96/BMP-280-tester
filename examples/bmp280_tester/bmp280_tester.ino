b4da75baa8c3a449620b656574b0f11f8777f0f6
Martin Chlebovec <martin.chlebovec.2@student.tuke.sk>
2019-03-13 21:29:34 +0100

    upload

--- examples/bmp280_tester/bmp280_tester.ino
+++ examples/bmp280_tester/bmp280_tester.ino
@@ -12,6 +12,8 @@ void loop() {
   float tlak = bmp.readPressure()/100;
   float nadmorska_vyska = bmp.readAltitude(1013.25);
   float tlak_hladina_mora = tlak / pow(1 - ((0.0065 *nadmorska_vyska) / (teplota + (0.0065 *nadmorska_vyska) + 273.15)), 5.257);
+  float tlak_hladina_mora2 = tlak / pow(1.0 - nadmorska_vyska / 44330.0, 5.255);
+  float tlak_hladina_mora3 = tlak / pow(1.0 - 0.0065 *nadmorska_vyska / (teplota + 273.15), 5.255);
   Serial.print("Teplota = ");
     Serial.print(teplota);
     Serial.println(" *C");
@@ -20,8 +22,17 @@ void loop() {
     Serial.print(tlak);
     Serial.println(" hPa");
 
-    Serial.print("Relativny tlak (prepocitany na hladinu mora s teplotou von)= ");
+    //RELATIVNY TLAK --> prepocitany na hladinu mora bosch vzorcom
+    Serial.print("Relativny tlak (origo Bosch vzorec) = ");
     Serial.print(tlak_hladina_mora);
+    Serial.println(" hPa");
+
+    Serial.print("Relativny tlak (upraveny Bosch vzorec 2 - najviac odpoveda skutocnosti z merani) = ");
+    Serial.print(tlak_hladina_mora2);
+    Serial.println(" hPa");
+
+    Serial.print("Relativny tlak (upraveny Bosch vzorec 3) = ");
+    Serial.print(tlak_hladina_mora3);
     Serial.println(" hPa");
 
     Serial.print("Nadmorska vyska = ");
